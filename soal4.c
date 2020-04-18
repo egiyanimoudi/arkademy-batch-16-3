ef64033d3e4d431750ce7e5d8646578735772b30
egiyanimoudi <63900716+egiyanimoudi@users.noreply.github.com>
2020-04-18 21:18:32 +0700

    penyelesaian soal 4

---
+++ soal4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+  unsigned char angka1;
+  unsigned short angka2;
+  unsigned long angka3;
+ 
+  angka1 = 56;
+  angka2 = 15946;
+  angka3 = 5956561594;
+
+  printf("Isi variabel angka1 (char): %d \n",angka1);
+  printf("Isi variabel angka2 (short) %d \n",angka2);
+  printf("Isi variabel angka3 (int): %u \n",angka3);
+ 
+  printf("\n");
+  return 0;
+}
