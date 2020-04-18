ae30009ba9a2b177f6d28d6f85f486fd9ca12754
egiyanimoudi <63900716+egiyanimoudi@users.noreply.github.com>
2020-04-18 19:18:00 +0700

    sebuah fungsi untuk membuat gambar segitiga sama kaki

---
+++ soal3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n;
+    
+    
+    cout << "masukkan panjang pola :";
+    cin >> n;
+    
+    cout <<"createTri(5)";
+    
+    
+	for (int i=1; i<=n; i++) {
+	    for(int j=n; j>i; j--) {
+			cout << " ";
+		}
+		for(int k=1; k <= (2*i - 1); k++) {
+		    	cout << "*";
+		}
+		cout << endl;
+	}
+	
+	cin.get();
+	return 0;
