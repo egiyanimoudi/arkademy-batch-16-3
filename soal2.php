ddbcf2390f8f223b165ae93666bc841a695016e7
egiyanimoudi <63900716+egiyanimoudi@users.noreply.github.com>
2020-04-18 17:40:10 +0700

    create soal no 2

---
+++ soal2.php
@@ -0,0 +1,18 @@
+<?php
+$username = '@egiyanimoudi';
+if(preg_match("/ani/", $username)) {
+  echo 'true';
+} else if (preg_match("/gamers/", $username)) {
+  echo 'false';
+}
+?>
+
+<?php
+$password = '@Moudi1904';
+if(preg_match("/i190/", $password)) {
+  echo 'true';
+} else if (pref_match("/456apa/", $password)){
+  echo 'false'; 
+}
+?>
+
